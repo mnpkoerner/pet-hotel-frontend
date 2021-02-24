d30adcf2a02460bf5f64ac1111991f552fca763a
collinradichel <radichel.collin@gmail.com>
2021-02-24 14:16:24 -0600

    continuing to build out Dashboard Component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function Dashboard() {
+  useEffect(() => {
+
+  }, []);
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {};
+
+  const handleChange = (event) => {
+    console.log("event.target.value", event.target.value);
+    setNewPet({ ...newPet, [event.target.name]: event.target.value });
+  };
+
+  const [newPet, setNewPet] = useState({
+    name: '',
+    color: '',
+    breed: '',
+    owner: ''
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pet Hotel</h1>
         <span>
-          <button>Dashboard</button>
-          <button>Manage Owners</button>
+          <button
+            onClick={() => {
+              history.push("/dashboard");
+            }}
+          >
+            Dashboard
+          </button>
+          <button
+            onClick={() => {
+              history.push("/owners");
+            }}
+          >
+            Manage Owners
+          </button>
         </span>
         <br />
         <h4>Add Pet</h4>
         <span>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <select></select>
-          <button>Submit</button>
+          <input onChange={handleChange} name="name" type="text" placeholder="Pet Name"/>
+          <input onChange={handleChange} name="color" type="text" placeholder="Pet Color"/>
+          <input onChange={handleChange} name="breed" type="text" placeholder="Pet Breed"/>
+          <select onChange={handleChange} name="owner" placeholder="Owner Name"></select>
+          <button onClick={handleSubmit}>Submit</button>
         </span>
         <br />
         <h4>History</h4>
@@ -33,6 +66,14 @@ function Dashboard() {
           </tr>
           <tr>
             <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <button>Delete</button>
+                <button>Check-In</button>
+            </td>
           </tr>
         </table>
       </header>
