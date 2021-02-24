0a136032c4f7b1d0909ce6c37321b9bf54445ef4
collinradichel <radichel.collin@gmail.com>
2021-02-24 14:21:07 -0600

    imported useDispatch

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function Dashboard() {
   useEffect(() => {
